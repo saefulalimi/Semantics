8296cd35fd724d12ec9eb3f0dcdadd38906b8929
saefulalimi <saefulalimi24@gmail.com>
2021-10-19 20:02:04 +0700

    styling chat with tailwind and css

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,7 +2,7 @@ import "../style/chat.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chatt from "../component/chat/Chatt";
-import loginchat from "../assets/loginchat.jpg";
+
 
 
 const socket = io.connect("http://localhost:5000");
@@ -26,7 +26,7 @@ function Chat() {
                 <div class="loginchat w-full sm:w-1/2 md:w-9/12 lg:w-1/2 mx-3 md:mx-5 lg:mx-0 shadow-md flex flex-col md:flex-row items-center rounded z-10 overflow-hidden bg-center bg-cover bg-blue-600">
                 {/* <img src={loginchat} alt="" srcset="" /> */}
                     <div class="w-full md:w-1/2 flex flex-col justify-center items-center bg-opacity-25 bg-yellow-700 backdrop">
-                        <h3 class="text-3xl md:text-4xl font-extrabold text-black my-2 md:my-0">
+                        <h3 class="mb-10 font-extrabold text-3xl flex items-center">
                             SEMANTICS
                         </h3>
                         <p class="mb-2 text-black hidden md:block font-mono">
@@ -54,10 +54,8 @@ function Chat() {
                                 type="text" 
                                 class="px-4 py-2 w-full rounded border border-gray-300 shadow-sm text-base placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:border-blue-500"
                             />
-                            <button onClick={joinRoom} class="flex justify-center items-center bg-yellow-700 hover:bg-yellow-300 text-Black focus:outline-none focus:ring rounded px-10 py-1">
-                                <p class="ml-1 text-lg">
-                                Join
-                                </p>
+                            <button onClick={joinRoom} class="px-10 py-1">
+                                <p class="text-Black font-bold text-lg focus:outline-none focus:ring border border-gray-300 w-full px-4 py-2 bg-blue-500 rounded">Join Chat</p>
                             </button>
                         </form>
                     </div>
