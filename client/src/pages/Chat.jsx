54b0f72a9233de99665853b85fa5e36fb7dd8a30
saefulalimi <saefulalimi24@gmail.com>
2021-10-19 18:20:14 +0700

    styling chat 1

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,6 +2,8 @@ import "../style/chat.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chatt from "../component/chat/Chatt";
+import loginchat from "../assets/loginchat.jpg";
+
 
 const socket = io.connect("http://localhost:5000");
 
@@ -17,32 +19,58 @@ function Chat() {
     }
   };
 
-  return (
-    <div className="App">
+  return(
+    <div>
+      <div class="bg-color h-screen w-full flex justify-center items-center bg-gradient-to-tr ">
       {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
-            type="text"
-            placeholder="Jhonn..."
-            onChange={(ev) => {
-              setUsername(ev.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room Id"
-            onChange={(ev) => {
-              setRoom(ev.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
-        </div>
-      ) : (
-        <Chatt socket={socket} username={username} room={room} />
-      )}
+                <div class="loginchat w-full sm:w-1/2 md:w-9/12 lg:w-1/2 mx-3 md:mx-5 lg:mx-0 shadow-md flex flex-col md:flex-row items-center rounded z-10 overflow-hidden bg-center bg-cover bg-blue-600">
+                {/* <img src={loginchat} alt="" srcset="" /> */}
+                    <div class="w-full md:w-1/2 flex flex-col justify-center items-center bg-opacity-25 bg-yellow-700 backdrop">
+                        <h3 class="text-3xl md:text-4xl font-extrabold text-black my-2 md:my-0">
+                            SEMANTICS
+                        </h3>
+                        <p class="mb-2 text-black hidden md:block font-mono">
+                        Lest Chat for Conversation
+                        </p>
+                    </div>
+                    <div class="w-full md:w-1/2 flex flex-col items-center bg-white py-5 md:py-8 px-4">
+                        <h3 class="mb-4 font-bold text-3xl flex items-center text-yellow-700">
+                        JOIN CHAT
+                        </h3>
+                        <form class="px-3 flex flex-col justify-center items-center w-full gap-3">
+                            <input 
+                                onChange={(ev) => {
+                                  setUsername(ev.target.value);
+                                }} 
+                                placeholder="Username..."
+                                type="text" 
+                                class="px-4 py-2 w-full rounded border border-gray-300 shadow-sm text-base placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:border-blue-500"
+                            />
+                            <input 
+                                onChange={(ev) => {
+                                  setRoom(ev.target.value);
+                                }} 
+                                placeholder="Room ID..."
+                                type="text" 
+                                class="px-4 py-2 w-full rounded border border-gray-300 shadow-sm text-base placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:border-blue-500"
+                            />
+                            <button onClick={joinRoom} class="flex justify-center items-center bg-yellow-700 hover:bg-yellow-300 text-Black focus:outline-none focus:ring rounded px-3 py-1">
+                                <svg class="w-5 h-5 inline"fill="none"stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
+                                </svg>
+                                <p class="ml-1 text-lg">
+                                JOIN
+                                </p>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                ) : (
+                  <Chatt socket={socket} username={username} room={room} />
+                )}
+            </div>
     </div>
-  );
+  )
 }
 
 export default Chat;
