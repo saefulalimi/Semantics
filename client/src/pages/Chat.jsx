30fc047d74b1493b5b9a0c6e6ef767b831e6ee7a
saefulalimi <saefulalimi24@gmail.com>
2021-10-20 08:51:20 +0700

    fix styling

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -24,7 +24,7 @@ function Chat() {
     <div>
       <div class="bg-color flex flex-1 items-center justify-center">
       {!showChat ? (
-                <div class="sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-1/2 text-center rounded-md">
+                <div class="sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-1/2 rounded-md text-center">
                     <div class="">
                         <h3 class="font-bold tracking-wider text-3xl mb-8 w-full text-black-600">
                             SEMANTICS
@@ -33,11 +33,11 @@ function Chat() {
                         Let's Chat for Conversation
                         </p>
                     </div>
-                    <div class="w-full md:w-1/2  flex flex-col items-center bg-white py-5 md:py-8 px-6">
+                    <div class="w-full md:w-1/2  flex flex-col items-center bg-white py-5 md:py-8 px-6 rounded-md  ">
                         {/* <h3 class="mb-4 font-bold text-3xl flex items-center text-yellow-700">
                         JOIN CHAT
                         </h3> */}
-                        <form class="text-center">
+                        <form class="space-y-4">
                             <input 
                                 onChange={(ev) => {
                                   setUsername(ev.target.value);
@@ -52,7 +52,7 @@ function Chat() {
                                 }} 
                                 placeholder="Room ID..."
                                 type="text" 
-                                class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700"
+                                class="px-4 py-2 w-full rounded border border-gray-300 shadow-sm text-base placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:border-blue-500"
                             />
                             <button onClick={joinRoom} class="px-10 py-1">
                                 <p class="text-Black font-bold text-lg focus:outline-none focus:ring border border-gray-300 w-full px-4 py-2 bg-blue-500 rounded-md">Join Chat</p>
